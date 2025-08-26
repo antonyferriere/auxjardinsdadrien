a9f61f4f846e5f47e18c3c0f343c738265351d37
Toony <antony.ferriere@gmail.com>
2025-08-27 01:31:29 +0200

    Bug package

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,15 @@
+const { minify } = require("terser");
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(require("eleventy-plugin-terser"));
-  eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-postcss"), {
-    plugins: [require("cssnano")]
+  eleventyConfig.addPlugin(require("terser"));
+  eleventyConfig.addPlugin(require("eleventy-plugin-postcss"), {
+    plugins: [require("cssnano")],
   });
 
   return {
     dir: {
       input: "src",
-      output: "build"
-    }
+      output: "build",
+    },
   };
 };
