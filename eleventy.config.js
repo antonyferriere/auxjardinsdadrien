7ad9c7261b382f9bd609e2f0e2feb6db64514d5b
Toony <antony.ferriere@gmail.com>
2025-08-27 02:36:44 +0200

    feat: add JS minification transform

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,16 @@
-const { minify } = require("terser");
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(require("terser"));
   eleventyConfig.addPlugin(require("eleventy-plugin-postcss"), {
     plugins: [require("cssnano")],
   });
+
+  eleventyConfig.addTransform("jsmin", async (content, path) => {
+    if (path.endsWith(".js")) {
+      return (await require("terser").minify(content)).code;
+    }
+    return content;
+  });
+
+  eleventyConfig.addPassthroughCopy("src/scripts");
 
   return {
     dir: {
